b6a925433d09719c66f638eeb8fd56eaa4ea7c16
Caio Oliveira <caiolv@me.com>
2020-04-16 16:03:23 -0300

    Configurar ReactoTron

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import './config/ReactotronConfig';
+
 export default function App() {
+  console.tron.log('teste');
   return (
     <View>
       <Text>Welcome to React Native.</Text>
