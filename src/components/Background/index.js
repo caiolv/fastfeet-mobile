556b69d1b251d137e5017edc5c8a82c19b2abec8
Caio Oliveira <caiolv@me.com>
2020-04-18 16:01:38 -0300

    Adicionar component de Background

--- src/components/Background/index.js
+++ src/components/Background/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import { View } from 'react-native';
+import styled from 'styled-components/native';
+import { Dimensions } from 'react-native';
+import SafeAreaView from 'react-native-safe-area-view';
 
-// import { Container } from './styles';
+export const Background = styled(SafeAreaView)`
+  flex: 1;
+  position: relative;
+  background: #fff;
+  padding: 110px 0;
+`;
 
-export default function Background() {
-  return <View />;
-}
+export const HeaderBackground = styled.View`
+  position: absolute;
+  background: #7d40e7;
+  height: 185px;
+  width: ${Dimensions.get('window').width}px;
+`;
